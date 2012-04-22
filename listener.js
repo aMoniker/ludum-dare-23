a5a8555d9a7783a170c69a30e5ecfef790ae90f4
Jim Greenleaf <james.a.greenleaf@gmail.com>
2012-04-22 19:07:00 -0400

    command lines

--- listener.js
+++ listener.js
@@ -64,7 +64,7 @@ io.sockets.on('connection', function (socket) {
       socket.emit('new_game_id', game_id);
 
       // make a child process running the game in server mode
-      cp.exec('node -h server.js ' + game_id, function (error, stdout, stderr) {
+      cp.exec('node server.js ' + game_id, function (error, stdout, stderr) {
         console.log('server.js for ' +game_id+ ' finished running', stdout);
       });
 
