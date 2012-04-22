ff17c96b2ab5c26f640ebd7be71aa1f0d9eee13f
Jim Greenleaf <james.a.greenleaf@gmail.com>
2012-04-22 19:32:11 -0400

    test

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ var game_id = arguments[0];
 console.log('game_id', game_id);
 
 var $ = require('jquery');
-
-console.log('$', $);
-
-//require('/var/www/ld/js/GameUtils.js');
-//console.log('required gameutils');
+require('/var/www/ld/js/GameUtils.js');
+console.log('required gameutils');
 
 var g = {
 
