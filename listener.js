a74121c9d8a6724b4b37f9ca9aa33493203d4f79
Jim Greenleaf <james.a.greenleaf@gmail.com>
2012-04-23 00:00:08 -0400

    ok

--- listener.js
+++ listener.js
@@ -64,13 +64,11 @@ io.sockets.on('connection', function (socket) {
       socket.emit('new_game_id', game_id);
 
       // make a child process running the game in server mode
-      var child = cp.spawn('node server.js ' + game_id, function (error, stdout, stderr) {
-        console.log('server.js for ' +game_id+ ' finished running', stdout);
-
-        if (error) {console.log('server.js error', error);}
-        if (stderr) {console.log('server.js stderr', stderr);}
-      });
+      var child = cp.spawn('node server.js ' + game_id);
       spawn.stdin.write('echo chamber?');
+      spawn.stdout.on('data', function (data) {
+        console.log('child data', data);
+      });
     });
 
     // test child process creation/destruction
